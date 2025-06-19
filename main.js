bcb98a24bf2f2f0e0c33bf7d4e2e40d269610daa
Hamza-mimoz <mimizou-hamza@hotmail.com>
2025-06-19 15:33:28 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,20 @@
+const divMenu = document.getElementById("menu_id");
+const btnOpen = document.getElementById("openId")
+const btnClose = document.getElementById("closeId")
+
+
+function btnMenu(){
+    if (divMenu.style.width == '100vw'){
+        divMenu.style.width = '0vw';
+        btnOpen.style.display = "block"
+        btnClose.style.display = "none"
+    }else{
+        divMenu.style.width = '100vw'
+        btnOpen.style.display = "none"
+        btnClose.style.display = "block"
+    }
+        
+    
+
+
+}
