61fc64fdf9c4f32870639015ad3fcb3b8e65e2e2
Hamza-mimoz <mimizou-hamza@hotmail.com>
2025-06-22 16:21:32 +0100

    we start with the product section

--- main.js
+++ main.js
@@ -17,8 +17,37 @@ function btnMenu(){
         btnClose.style.display = "block"
         
     }
-        
-    
+}
 
+
+
+const Pbtn1 = document.getElementById("btn1")
+const Pbtn2 = document.getElementById("btn2")
+const Pbtn3 = document.getElementById("btn3")
+const Pbtn4 = document.getElementById("btn4")
+
+
+function toggleClass(btn){
+ if(btn == Pbtn1){
+    Pbtn1.classList.add("active")
+    Pbtn2.classList.remove("active")
+    Pbtn3.classList.remove("active")
+    Pbtn4.classList.remove("active")
+ }else if(btn == Pbtn2){
+    Pbtn2.classList.add("active")
+    Pbtn1.classList.remove("active")
+    Pbtn3.classList.remove("active")
+    Pbtn4.classList.remove("active")
+ }else if(btn == Pbtn3){
+    Pbtn3.classList.add("active")
+    Pbtn2.classList.remove("active")
+    Pbtn1.classList.remove("active")
+    Pbtn4.classList.remove("active")
+ }else if(btn == Pbtn4){
+    Pbtn4.classList.add("active")
+    Pbtn2.classList.remove("active")
+    Pbtn3.classList.remove("active")
+    Pbtn1.classList.remove("active")
+ }
 
 }
